4c3cf927ba2031276b98933823f8faf94cff91d4
Michael Yuen <michael@overimagined.com>
2025-05-14 23:16:08 +0800

    Parse draw command

--- Samples/OpenTKSVG/SVGParser.cs
+++ Samples/OpenTKSVG/SVGParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AngleSharp.Dom;
 using AngleSharp.Html.Parser;
@@ -12,7 +13,7 @@ namespace OpenTKSVG
 {
     class SVGParser
     {
-        public static List<string> ParseAndDraw(Canvas canvas, string svg)
+        public static void ParseAndDraw(Canvas canvas, string svg)
         {
             var parser = new HtmlParser();
             var document = parser.ParseDocument(svg);
@@ -25,14 +26,46 @@ namespace OpenTKSVG
                 if (!string.IsNullOrEmpty(dAttribute))
                 {
                     pathDataList.Add(dAttribute);
+                    Draw(canvas, dAttribute);
                 }
             }
-            return pathDataList;
         }
 
         public static void Draw(Canvas canvas, string pathData)
         {
             canvas.BeginPath();
+
+            Console.WriteLine(pathData);
+
+            var commandList = new List<string>();
+            if (!string.IsNullOrEmpty(pathData))
+            {
+                var matches = Regex.Matches(pathData, @"([a-zA-Z])([^a-zA-Z]*)");
+                foreach (Match match in matches)
+                {
+                    commandList.Add(match.Groups[1].Value + match.Groups[2].Value.Trim());
+                }
+            }
+
+            // At this point, commandList contains strings like "M150 5", "L75 200", "L225 200", "Z"
+            foreach (var commandSegment in commandList)
+            {
+                var command = commandSegment[0];
+
+                switch(command)
+                {
+                    case 'M':
+                        //canvas.MoveTo()
+                        break;
+                    case 'L':
+                        //canvas.MoveTo()
+                        break;
+                    case 'Z':
+                        //canvas.MoveTo()
+                        break;
+                }
+                Console.WriteLine($"Command segment: {commandSegment}");
+            }
         }
     }
 }
